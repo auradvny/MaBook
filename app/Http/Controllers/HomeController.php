ad65b41901a9a9aab29bdcf19279e44ddbc8f3f9
auradvny <auradvny14@gmail.com>
2024-07-02 16:13:21 +0700

    tampilkan data kategori buku

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\KategoriBuku;
 use App\Models\Buku;
 
@@ -8,19 +9,19 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        $KategoriBuku= KategoriBuku::Get();
-        
+    public function index()
+    {
 
-        return view('admin.dashboard', compact('KategoriBuku'));
+        $KategoriBuku = KategoriBuku::orderBy('id_kategori', 'desc')->get();
+        $total = KategoriBuku::count();
+        return view('admin.dashboard', compact(['KategoriBuku', 'total']));
     }
-    
-    public function tampil(){
-        $KategoriBuku= KategoriBuku::Get();
-        
+
+    public function tampil()
+    {
+        $KategoriBuku = KategoriBuku::Get();
+
 
         return view('dashboard', compact('KategoriBuku'));
     }
-   
-   
 }
