5b4bf89b4ec068c12ec33c6094034dd43538da3e
Dwita18 <dwitaisma18@gmail.com>
2024-07-03 14:06:22 +0700

    edit tambah buku

--- resources/views/admin/Buku/create.blade.php
+++ resources/views/admin/Buku/create.blade.php
@@ -14,7 +14,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900">
                    <h1 class="mb-0"> Tambah Buku </h1>
 
                    <form action="{{ route('admin.Buku.submit') }}" method="POST">
