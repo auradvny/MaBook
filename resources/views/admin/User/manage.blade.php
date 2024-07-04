a378b6f5e48e47fca240a877f0f1ab9541feea84
Dwita18 <dwitaisma18@gmail.com>
2024-07-04 09:26:07 +0700

    edit tabel user

--- resources/views/admin/User/manage.blade.php
+++ resources/views/admin/User/manage.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Aplikasi Perpustakaan</title>
-        <link rel ="stylesheet" href='{{ asset('bootstrap/css/bootstrap.css') }}'>
+        <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
     </head>
 
     <body>
@@ -16,39 +16,42 @@
                 {{ __('User') }}
             </h2>
         </x-slot>
-        <div class ="mt-3 container">
-            <h4> List User </h4>
-            <div class="ms-auto">
-                <a class="btn btn-success" href='{{ route('admin.User.create') }}'>Tambah</a>
+        <div class="container mt-3">
+            <h4>List User</h4>
+            <div class="d-flex justify-content-end mb-3">
+                <a class="btn btn-success" href="{{ route('admin.User.create') }}">Tambah</a>
             </div>
-
-            <Table class ="table">
-                <thead>
+            <table class="table table-striped table-bordered">
+                <thead class="table-dark">
                     <tr>
-                        <th> No </th>
-                        <th> Nama</th>
-                        <th> Email</th>
-                        <th> Usertype </th>
-                        <th> Aksi </th>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Usertype</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
-                <?php $no = 1; ?>
-                @foreach ($User as $data)
-                    <tr>
-                        <td> {{ $no++ }}</td>
-                        <td> {{ $data->name }}</td>
-                        <td>{{ $data->email }} </td>
-                        <td> {{ $data->usertype }}</td>
-                        <td>
-                            <a href="{{ route('admin.User.edit', $data->id) }}" class="btn btn-sm btn-warning">Edit</a>
-
-                            <form action="{{ route('admin.User.delete', $data->id) }}" method="POST">
-                                @csrf
-                                <button class="btn btn-sm btn-danger"> Hapus </button>
-                            </form>
-                    </tr>
-                @endforeach
-            </Table>
+                <tbody>
+                    <?php $no = 1; ?>
+                    @foreach ($User as $data)
+                        <tr>
+                            <td>{{ $no++ }}</td>
+                            <td>{{ $data->name }}</td>
+                            <td>{{ $data->email }}</td>
+                            <td>{{ $data->usertype }}</td>
+                            <td class="d-flex">
+                                <a href="{{ route('admin.User.edit', $data->id) }}" class="btn btn-sm btn-warning me-2">Edit</a>
+                                <form action="{{ route('admin.User.delete', $data->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    <button class="btn btn-sm btn-danger">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </body>
+
+    </html>
 </x-app-layout>
